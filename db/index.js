5811ba877c97886ac8bb94ea86f668b5fe1db947
Salissa <salissa4@gmail.com>
2022-10-25 08:29:18 -0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -5,50 +5,6 @@ class DB {
       this.connection = connection;
     }
 
-// view all employees
-allEmployees() {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-    );
-  }
-
-// view all manager
-allManagers(employeeId) {
-  return this.connection.promise().query(
-    "SELECT id, first_name, last_name FROM employee WHERE id != ?",
-    employeeId
-    );
-  }
-
-// view all roles
-allRoles() {
-  return this.connection.promise().query(
-    "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-  );
-}
-
-// view all departments
-allDepartments() {
-  return this.connection.promise().query(
-    "SELECT department.id, department.name FROM department;"
-  );
-}
-
-// view all emp by dept
-allEmployeesByDepartment(departmentId) {
-  return this.connection.promise().query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
-    departmentId
-  );
-}
-
-// view dept budget
-viewDepartmentBudget() {
-  return this.connection.promise().query(
-    "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
-  );
-}
-
 //create employee
 addEmployee(employee) {
   return this.connection.promise().query(
